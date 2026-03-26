202267c676c257ecfc3d967079d371ab4bbdda40
nov05 <1215590+nov05@users.noreply.github.com>
2026-03-26 11:24:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello GCP."}
+    model = {"title": "Hello, App Engine!"}
     return render_template('index.html', model=model)
 
 # @app.route("/")
